3db6baa465d84ee56737863f36994c1b5f40dca5
mehrit2003 <mehrit2003@gmail.com>
2023-02-25 18:15:22 +0100

    appendChild() h3 in div

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ para.style.color = ("red");
 const theDiv = document.getElementById("div1");
 theDiv.appendChild(para);
 
+const head3 = document.createElement("h3");
+head3.textContent = "I'm a blue h3!";
+theDiv.appendChild(head3);
+
 const btn = document.createElement("button");
 btn.innerHTML = "Hello Button";
 
