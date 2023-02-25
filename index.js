ae22d8f6c030d1df121b203f58dd54f17fdb3960
mehrit2003 <mehrit2003@gmail.com>
2023-02-25 17:48:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const para = document.createElement("p");
+para.textContent = "Hey, I'm Red!";
+para.style.color = ("red");
+const theDiv = document.getElementById("div1");
+theDiv.appendChild(para);
+
+const btn = document.createElement("button");
+btn.innerHTML = "Hello Button";
+
+console.log("Red Text");
+
