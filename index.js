3584e7f3e29a00a1b6d53feec634c1ff3f51b491
mehrit2003 <mehrit2003@gmail.com>
2023-02-26 15:31:53 +0100

    createElement h3, style it and append it to body

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//get body
+const body = document.getElementById("body");
+console.log(body);
+
 //red text p
 const para = document.createElement("p"); //create paragraph named "para"
 /*para.style.width = "200px"     //styling the p box
@@ -5,5 +9,12 @@ para.style.height = "200px"
 para.style.backgroundColor = "red";*/
 para.style.color = "red"; // coloring text
 para.textContent = "Hey, I'm red text"; //textContent
-document.body.appendChild(para);  //insert para in the html body
+body.appendChild(para);  //insert para in the html body
 console.log(para);
+
+
+//h3 text
+const heading3 = document.createElement("h3");
+heading3.style.color = "blue";
+heading3.textContent = "I'm blue h3!";
+body.appendChild(heading3);
