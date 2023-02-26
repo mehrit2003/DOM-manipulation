71a3069d3c56e667f9ae7d1bddc011a137377522
mehrit2003 <mehrit2003@gmail.com>
2023-02-26 15:26:07 +0100

    createElement para, style it and append it to body

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
-//get the body 
-const body = document.getElementById("body");
-
-
-
-
-const para = document.createElement("p");
-para.textContent = "Hey, I'm Red!";
-para.style.color = ("red");
-const div1 = document.getElementById("div1");
-div1.appendChild(para);
-
-const head3 = document.createElement("h3");
-head3.color = "blue"
-head3.textContent = "I'm a blue h3!"; //textContent
-
-head3.style.color = "blue"; //Styling color
-
-//////////////////////////////////
-
-const div2 = document.createElement("div");
-div2.style.borderColor = "black";
-div2.style.backgroundColor = "pink";
-div2.style.color = "green";
-div2.style.height = 500
-
-
-
-div1.appendChild(para);
-
-
-body.appendChild(head3); //appendChild in Parent
-
-console.log(body);
-
+//red text p
+const para = document.createElement("p"); //create paragraph named "para"
+/*para.style.width = "200px"     //styling the p box
+para.style.height = "200px"
+para.style.backgroundColor = "red";*/
+para.style.color = "red"; // coloring text
+para.textContent = "Hey, I'm red text"; //textContent
+document.body.appendChild(para);  //insert para in the html body
+console.log(para);
