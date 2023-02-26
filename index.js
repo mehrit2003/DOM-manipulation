cb871d6a2ccd5ca0e865d19df53ec4f97b3e394a
mehrit2003 <mehrit2003@gmail.com>
2023-02-26 14:54:15 +0100

    HHow to get body element?

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//get the body 
+const body = document.getElementById("body");
+
+
+
+
 const para = document.createElement("p");
 para.textContent = "Hey, I'm Red!";
 para.style.color = ("red");
-const theDiv = document.getElementById("div1");
-theDiv.appendChild(para);
+const div1 = document.getElementById("div1");
+div1.appendChild(para);
 
 const head3 = document.createElement("h3");
 head3.color = "blue"
@@ -10,14 +16,20 @@ head3.textContent = "I'm a blue h3!"; //textContent
 
 head3.style.color = "blue"; //Styling color
 
-theDiv.appendChild(head3); //appendChild in Parent
+//////////////////////////////////
+
+const div2 = document.createElement("div");
+div2.style.borderColor = "black";
+div2.style.backgroundColor = "pink";
+div2.style.color = "green";
+div2.style.height = 500
 
 
 
+div1.appendChild(para);
 
 
-const btn = document.createElement("button");
-btn.innerHTML = "Hello Button";
+body.appendChild(head3); //appendChild in Parent
 
-console.log("Red Text");
+console.log(body);
 
