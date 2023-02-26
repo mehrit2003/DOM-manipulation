381e48e840d4d1def2bb42ce7133f51073a15fea
mehrit2003 <mehrit2003@gmail.com>
2023-02-26 15:48:05 +0100

    Added firstDiv, create its styled content and append it Last to the body

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ const heading3 = document.createElement("h3");
 heading3.style.color = "blue";
 heading3.textContent = "I'm blue h3!";
 body.appendChild(heading3);
+
+
+//create styled firstDiv and append ir to body after adding it's content
+const firstDiv = document.createElement("div");
+firstDiv.style.backgroundColor = "pink";
+//body.appendChild(firstDiv);   
+
+
+
+//nested h1 in firstDiv
+const nestedH1 = document.createElement("h1");
+nestedH1.textContent = "I'm in div"
+
+firstDiv.appendChild(nestedH1) // append h1 in the firstDiv
+
+//nested p in firstDiv
+const nestedPara = document.createElement("p");
+nestedPara.textContent = "ME TOO!";
+firstDiv.appendChild(nestedPara);
+
+
+//append firstDiv to body
+body.appendChild(firstDiv);
+
+
